734cb7af365b375933405a15dc456289e20dfc46
koichi210 <koichi210@gmail.com>
2020-11-12 23:36:11 +0900

    ロギングデータ更新

--- System/TimeStump.bat
+++ System/TimeStump.bat
@@ -3,4 +3,4 @@ set OUTPUT_FILE="C:\DATA\tmp\TimeStump.txt"
 set DATE_STR=%date:/=%
 set TIME_STR=%time:/=%
 
-echo %DATE_STR%_%TIME_STR% >> %OUTPUT_FILE% %1
+echo %DATE_STR%_%TIME_STR% %1 >> %OUTPUT_FILE%
